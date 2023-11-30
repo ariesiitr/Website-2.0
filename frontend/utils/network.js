7d85730025d00f5d781d4f934acce51d741b6d57
paaban25 <paaban25@gmail.com>
2023-11-30 11:30:06 +0530

    teams project menu aboutus contact created

--- frontend/utils/network.js
+++ frontend/utils/network.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-
 export function addQueryParams(urlString, queryParams) {
   const query = Object.keys(queryParams)
     .map((k) => {
@@ -13,30 +12,29 @@ export function addQueryParams(urlString, queryParams) {
   return `${urlString}?${query}`;
 }
 
-const addClientIdToBody = body => {
-  
-    return {
-      ...body,
-      client_id: process.env.REACT_APP_CLIENT_ID,
-      client_secret: process.env.REACT_APP_CLIENT_SECRET,
-    };
-  }
-
+const addClientIdToBody = (body) => {
+  return {
+    ...body,
+    client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+    client_secret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+  };
+};
 
 export function request(
   method,
   url,
   data,
   authorized = true,
-  contentType = "application/json"
+  contentType = "application/json",
 ) {
-
   return new Promise(async (resolve, reject) => {
     let headers = { "content-type": contentType };
- 
+    const body = addClientIdToBody(data);
+
     if (authorized) {
       const token = await getKey(TOKEN_TYPE);
-      const body = addClientIdToBody(data);
+      // const body = addClientIdToBody(data);
+      // console.log("body =>",body);
       if (token) {
         headers.Authorization = `Bearer ${token}`;
         axios({
@@ -50,13 +48,13 @@ export function request(
             resolve(res);
           })
           .catch((err) => {
-          
             reject(err);
           });
       } else {
         reject("Unauthorized");
       }
     } else {
+     
       axios({
         method,
         url,
@@ -68,7 +66,6 @@ export function request(
           resolve(res);
         })
         .catch((err) => {
-        
           reject(err);
         });
     }
@@ -91,12 +88,12 @@ export const getErrorBody = (error) => {
 };
 
 const get_base_api = () => {
-  let env = process.env.NODE_ENV;
-  if (env == 'production') return process.env.PRODUCTION_SERVER;
-  else if (env == 'staging')
-    return process.env.STAGING_SERVER;
-  
-  return process.env.DEVELOPMENT_SERVER;
+  // let env = process.env.NODE_ENV;
+  // if (env == 'production') return process.env.PRODUCTION_SERVER;
+  // else if (env == 'staging')
+  //   return process.env.STAGING_SERVER;
+
+  return process.env.NEXT_PUBLIC_DEV_SERVER;
 };
 
 export const BASE_API = get_base_api();
@@ -106,7 +103,7 @@ function getBaseImgUrl() {
   let env = process.env.NODE_ENV;
   if (env === "production" || env === "staging" || env === "dev") return "";
   else {
-    return process.env.DEVELOPMENT_SERVER;
+    return process.env.NEXT_PUBLIC_DEV_SERVER;
   }
 }
 
