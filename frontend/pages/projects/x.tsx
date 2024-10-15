ba95f69f1447f88c8897289098dac523bb39db3e
Deepak <deepak988088@gmail.com>
2024-10-15 23:14:33 +0530

    sponserpage done

---
+++ frontend/pages/projects/x.tsx
@@ -0,0 +1,16 @@
+// components/Projects.js
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <section className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            {/* Title */}
+            <h2 className="mb-6 text-3xl font-bold">Our Projects</h2>
+
+            {/* Project Cards */}
+        
+        </section>
+    );
+};
+
+export default Projects;
